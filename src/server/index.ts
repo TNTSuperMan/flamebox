04ae8cd549f38d3d92aa128e3ae61c77099127ce
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-07-14 18:25:11 +0900

    例外時にonError(Error)を返す

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,14 +11,18 @@ export const flame = <T extends FlameRoutes>(
     const routes_map = flameroutesToMap(routes);
 
     return async req => {
-        const req_json = await req.json().catch(() => undefined);
-        if (req_json === undefined) {
-            return await onError({ type: "BadJSON" }, req);
+        try {
+            const req_json = await req.json().catch(() => undefined);
+            if (req_json === undefined) {
+                return await onError({ type: "BadJSON" }, req);
+            }
+            const { pathname } = new URL(req.url);
+            const route = routes_map.get(pathname);
+            return route
+                ? new Response(JSON.stringify(await route(req_json)))
+                : await onError({ type: "NotFound" }, req);
+        } catch (error) {
+            return await onError({ type: "Error", error }, req);
         }
-        const { pathname } = new URL(req.url);
-        const route = routes_map.get(pathname);
-        return route
-            ? new Response(JSON.stringify(await route(req_json)))
-            : await onError({ type: "NotFound" }, req);
     }
 }
