0168f624de01f037569bf8e5ee6d6b04a3ba3c3c
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-07-29 16:02:05 +0900

    ルートマップの構造を改良

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,3 @@
-import { flameroutesToMap, type MappedFlameRoutes } from "./map";
 import type { FlameRoute, FlameRoutes } from "./types";
 
 export type FlameFetch<R extends FlameRoutes> = (req: Request) => Promise<Response>;
@@ -6,24 +5,29 @@ export type FlameFetch<R extends FlameRoutes> = (req: Request) => Promise<Respon
 const r404 = () => new Response(null, { status: 404 });
 
 export const flame = <T extends FlameRoutes>(routes: T): FlameFetch<T> => {
-  const routes_map = flameroutesToMap(routes);
+  const routes_map = new Map<string, FlameRoute<any, any>>;
+
+  const visit = (r: FlameRoutes, prefix: string) => {
+    for (const key in r) {
+      const path = prefix + "/" + key;
+      if (typeof r[key] === "function") {
+        routes_map.set(path, r[key]);
+      } else {
+        visit(r[key]!, path);
+      }
+    }
+  }
+
+  visit(routes, "");
 
   return async req => {
     const req_json = await req.json().catch(() => undefined);
     if (req_json === undefined) {
       return r404();
     }
-    let curr_route: MappedFlameRoutes | FlameRoute<any, any> = routes_map;
-    for (const r of new URL(req.url).pathname.slice(1).split("/")) {
-      if (typeof curr_route === "object") {
-        curr_route = curr_route.get(r)!;
-        if (!curr_route) {
-          return r404();
-        }
-      }
-    }
-    return typeof curr_route === "object"
-      ? r404()
-      : new Response(JSON.stringify(await curr_route(req_json)))
+    const handler = routes_map.get(new URL(req.url).pathname);
+    return handler
+      ? new Response(JSON.stringify(await handler(req_json)))
+      : r404();
   }
 }
