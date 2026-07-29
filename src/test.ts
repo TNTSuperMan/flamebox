aebbda62781ad13bead622456059349ed447663c
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-07-29 16:07:42 +0900

    テストコードのインデントもなおす

--- src/test.ts
+++ src/test.ts
@@ -2,18 +2,18 @@ import { flame as flameC } from "./client";
 import { flame as flameS } from "./server";
 
 const fetch = flameS({
-    async login({ username, password }: { username: string, password: string }) {
-        if (username === "tnt" && password === "tntn7095110") {
-            return {
-                type: "success",
-                token: crypto.randomUUID(),
-            } as const;
-        } else {
-            return {
-                type: "failed",
-            } as const;
-        }
+  async login({ username, password }: { username: string, password: string }) {
+    if (username === "tnt" && password === "tntn7095110") {
+      return {
+        type: "success",
+        token: crypto.randomUUID(),
+      } as const;
+    } else {
+      return {
+        type: "failed",
+      } as const;
     }
+  }
 });
 
 //@ts-ignore
